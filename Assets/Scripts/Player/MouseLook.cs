b8b1a808b77e69f8fbeb0c21589f89b7efbea468
Star <101126792+Starx707@users.noreply.github.com>
2024-03-14 20:26:25 +0100

    Added Asset
    This asset is for 3rd person player controler

--- Assets/Scripts/Player/MouseLook.cs
+++ Assets/Scripts/Player/MouseLook.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class MouseLook : MonoBehaviour
 {
-    public float CamSensitivity = 300f;
+    public float CamSensitivity = 900f;
     public Transform playerBody;
     float XRotation = 0f;
 
